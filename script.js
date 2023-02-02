99d3b4d539e417a842159e1a0647e64aed4ee289
DanSouz <lkutuzoffv@mail.ru>
2023-02-02 15:58:17 +0500

    fixed files

--- script.js
+++ script.js
@@ -11,11 +11,22 @@ inputs.forEach(input => {
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
+
     if (document.getElementById('1').checked) {
-        renderFirstLevelScreen();
+        window.level = {
+            level: inputs[0].id
+        };
+        window.renderFirstLevelScreen();
     } else if (document.getElementById('2').checked) {
-        renderSecondLevelScreen()
+        window.level = {
+            level: inputs[1].id
+        };
+        window.renderSecondLevelScreen();
     } else if (document.getElementById('3').checked) {
-        renderThirdLevelScreen()
+        window.level = {
+            level: inputs[2].id
+        };
+        window.renderThirdLevelScreen();
     }
+    console.log(`${window.level.level} уровень`);
 });
