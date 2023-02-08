0774e7496e7c246b8fe2474eb381cdfa4b89d683
DanSouz <lkutuzoffv@mail.ru>
2023-02-08 21:43:58 +0500

    noot

--- script.js
+++ script.js
@@ -17,12 +17,15 @@ btn.addEventListener("click", (e) => {
 
   if (document.getElementById("1").checked) {
     renderLevelScreen("1");
+    renderCardsScreen("3");
     window.application.level = inputs[0].id;
   } else if (document.getElementById("2").checked) {
     renderLevelScreen("2");
+    renderCardsScreen("6");
     window.application.level = inputs[1].id;
   } else if (document.getElementById("3").checked) {
     renderLevelScreen("3");
+    renderCardsScreen("9");
     window.application.level = inputs[2].id;
   }
 });
